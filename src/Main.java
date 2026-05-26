33416087912989678a1f8034df56d33de1b143a0
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-05-26 14:20:09 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//1. Girar
+//2. Loja
+//3. Estatísticas
+//4. Sair
+// vai ter um txt que guarda as estatísticas.
+// uma loja para comprar items (o dinheiro também será guardado em um txt para sempre se ter
+// caso esteja devendo deverá emprestar do banco com juros muito altos mas haverá a possibilidade de pedir para um agiota, com menos juros, com riscos, é claro.
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Double saldoAtual;
+
+        System.out.println("********************************");
+        System.out.println("Bem vindo ao Cassino do Bicheiro");
+        System.out.println("********************************");
+
+        System.out.println("""
+                1. Girar
+                2. Loja
+                3. Estatísticas
+                4. Sair
+                """);
+
+
+
+    }
+}
